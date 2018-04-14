de54aeba0c5ee33cd42a6dafa733bf87349e2473
WilderGao <845956752@qq.com>
2018-04-14 13:43:20 +0800

    供电局项目

---
+++ src/main/java/com/wilder/power_supply/PowerSupplyApplication.java
@@ -0,0 +1,12 @@
+package com.wilder.power_supply;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PowerSupplyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PowerSupplyApplication.class, args);
+	}
+}
